d3f856f5b804fd05e156fcdad2c395a0a44c1f9a
HeciarMask <franheciar@gmail.com>
2022-09-26 14:42:53 +0200

    HDL projecto 2 ini

---
+++ HDL/Saludo_Dinamico/src/java/Datos.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author alumno
+ */
+public class Datos {
+    
+    private String nombre;
+    /**
+     * Creates a new instance of Datos
+     */
+    public Datos() {
+    }
+
+    public String getNombre() {
+        return nombre;}
+    public void setNombre(String nombre) {
+        this.nombre = nombre;}
+    
+}
