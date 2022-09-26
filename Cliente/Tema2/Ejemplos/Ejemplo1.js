25fc6ca705512b6779cfb19ff2c38217e2dd362d
HeciarMask <franheciar@gmail.com>
2022-09-26 14:00:53 +0200

    cliente ejemplos

--- Cliente/Tema2/Ejemplos/Ejemplo1.js
+++ Cliente/Tema2/Ejemplos/Ejemplo1.js
@@ -1,3 +1,4 @@
-"use strict";
-
-let intEdadAlumno;
+let numOperador1 = prompt("Introduzca el primer número");
+let numOperador2 = prompt("introduzca el segundo número");
+let total = Number(numOperador1) + Number(numOperador2);
+alert(`${numOperador1} + ${numOperador2} = ${total}`);
---
+++ Cliente/Tema2/Ejemplos/Ejemplo2.js
@@ -0,0 +1,8 @@
+let booleanJefe = confirm('¿Eres el jefe?');
+let respuesta;
+if(booleanJefe)
+    respuesta = "Sí, eres el jefe.";
+else
+    respuesta = "Eres trabajador";
+alert(respuesta);
+console.log(booleanJefe + "  " + respuesta);
