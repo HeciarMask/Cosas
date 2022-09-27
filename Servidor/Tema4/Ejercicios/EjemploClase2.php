0acca878b2f93e0cc573d0f90be5d9d7c49bf965
HeciarMask <franheciar@gmail.com>
2022-09-27 11:26:27 +0200

    foreach indice

--- Servidor/Tema4/Ejercicios/EjemploClase2.php
+++ Servidor/Tema4/Ejercicios/EjemploClase2.php
@@ -2,8 +2,9 @@
     $a[4] = 1;
     $a[-3] = 2;
     $a[2] = 3;
+    $a[] = 4;
 
-    foreach($a as $valor){
-        echo $valor;
+    foreach($a as $indice => $valor){
+        echo $indice." => ".$valor."<br>";
     }
 ?>
