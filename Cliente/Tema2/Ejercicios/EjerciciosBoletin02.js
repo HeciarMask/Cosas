5c986ca29ae87fba3536b418adc273f1c3a0e290
HeciarMask <franheciar@gmail.com>
2022-09-28 10:48:28 +0200

    cliente empieza boletin 2

---
+++ Cliente/Tema2/Ejercicios/EjerciciosBoletin02.js
@@ -0,0 +1,18 @@
+/*  1. Cree un programa que almacene en variables el valor de la edad, el nombre y el
+estado civil de una persona y a continuación lo muestre por pantalla.   */
+
+function ejercicio01() {
+  let edad = prompt("Introduzca su Edad:");
+  let nombre = prompt("Introduzca su Nombre:");
+  let eCivil = prompt("Introduzca su Estado Civil:");
+
+  console.log(
+    "Bienvenido: " +
+      nombre +
+      ". Su estado civil es " +
+      eCivil +
+      " y su edad es " +
+      edad +
+      "."
+  );
+}
