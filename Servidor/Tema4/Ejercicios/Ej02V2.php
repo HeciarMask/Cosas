2fa9d8e40ef74ca70a81828939683bd9d10f1901
HeciarMask <franheciar@gmail.com>
2022-09-29 14:47:19 +0200

    servidor

--- Servidor/Tema4/Ejercicios/Ej02V2.php
+++ Servidor/Tema4/Ejercicios/Ej02V2.php
@@ -2,35 +2,56 @@
 include("notas.php");
 if (isset($_POST['alumno'])) {
     $seleccionado = $_POST['alumno'];
+    if($seleccionado == "Todos"){?>
+        <!DOCTYPE html>
+        <html lang="en">
+
+        <head>
+            <meta charset="UTF-8">
+            <meta http-equiv="X-UA-Compatible" content="IE=edge">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>Document</title>
+        </head>
+
+        <body>
+            <table border="solid">
+                <?php
+                    echo "not not";
+                ?>
+            </table>
+        </body>
+
+        </html>
+    <?php}elseif($seleccionado != "Todos"){
     ?>
-<!DOCTYPE html>
-<html lang="en">
+    <!DOCTYPE html>
+    <html lang="en">
 
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+    </head>
 
-<body>
-    <table border="solid">
-        <?php
-            $media = 0;
-            echo "<h2>Las notas de $seleccionado</h2>";
-            foreach($notas[$seleccionado] as $asignatura => $nota){
-                echo "<tr><th>$asignatura</th><th>$nota</th></tr>";
-                $media += $nota;
-            }
-            $media = $media / count($notas[$seleccionado]);
-            echo "<tr><th>Nota Media</th><th>".$media."</th></tr>";
-        ?>
-    </table>
-</body>
+    <body>
+        <table border="solid">
+            <?php
+                $media = 0;
+                echo "<h2>Las notas de $seleccionado</h2>";
+                foreach($notas[$seleccionado] as $asignatura => $nota){
+                    echo "<tr><th>$asignatura</th><th>$nota</th></tr>";
+                    $media += $nota;
+                }
+                $media = $media / count($notas[$seleccionado]);
+                echo "<tr><th>Nota Media</th><th>".$media."</th></tr>";
+            ?>
+        </table>
+    </body>
 
-</html>
+    </html>
 <?php
-} else { ?>
+}} else { ?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -45,6 +66,7 @@ if (isset($_POST['alumno'])) {
     
     <form name="notas" action="Ej02V2.php" method="post">
         Selecciona nombre del alumno: <select name="alumno">
+            <option selected>Todos</option>
             <?php
                 foreach ($notas as $alumno => $lista)
                     echo "<option>$alumno</option>";
---
+++ Servidor/Tema4/Ejercicios/Ej03.php
@@ -0,0 +1,29 @@
+<?php
+// Para rellenar la primitiva queremos visualizar un array de seis elementos, conteniendo cada celda un
+// número aleatorio comprendido entre 1 y 49 en la que habremos de evitar la posibilidad de que un número se
+// repita dos veces
+$numeros = array();
+
+for($i = 1; $i <= 49; $i++){
+    $numeros[] = $i;
+}
+shuffle($numeros);
+for($j = 1; $j <= 6; $j++){
+    echo "Numero $j: ".$numeros[$j]."<br>";
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form name="lista" action="Ej03.php" method="post">
+        <input type="submit" value="Recalcular">
+    </form>
+    
+</body>
+</html>
