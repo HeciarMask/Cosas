4883b8128ac3f0fa2b36f69ff7a10b1345fb692a
HeciarMask <franheciar@gmail.com>
2022-09-30 09:29:19 +0200

    php ejs

--- Servidor/Tema4/Ejercicios/Ej03.php
+++ Servidor/Tema4/Ejercicios/Ej03.php
@@ -2,11 +2,7 @@
 // Para rellenar la primitiva queremos visualizar un array de seis elementos, conteniendo cada celda un
 // número aleatorio comprendido entre 1 y 49 en la que habremos de evitar la posibilidad de que un número se
 // repita dos veces
-$numeros = array();
-
-for($i = 1; $i <= 49; $i++){
-    $numeros[] = $i;
-}
+$numeros = range(1,49);//Antes tenia un bucle
 shuffle($numeros);
 for($j = 1; $j <= 6; $j++){
     echo "Numero $j: ".$numeros[$j]."<br>";
--- Servidor/Tema4/Ejercicios/Ej06/por_nombre.php
+++ Servidor/Tema4/Ejercicios/Ej06/por_nombre.php
@@ -1,5 +1,50 @@
 <?php
 include ("arrays.php");
 
-
-?>
+if(isset($_POST['nombre'])){
+$alumno = $_POST['nombre'];?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    Selección: <?php echo $alumno; ?>
+        <table border="solid">
+        <tr><th>Ciudades Visitadas</th></tr>
+        <?php
+            foreach($arraygeneral[$alumno] as $j){
+                echo "<tr><th>$j</th></tr>";
+            }
+        ?>
+    </table>
+</body>
+</html>
+<?php
+}else{?>
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+    </head>
+    <body>
+        <form name="nombres" action="por_nombre.php" method="post">
+            Usuario: <select name="nombre">
+                <?php
+                    foreach($arraynombres as $i){
+                        echo "<option>$i</option>";
+                    }
+                ?>
+            </select>
+            <input type="submit" value="Consultar">
+        </form>
+    </body>
+    </html>
+<?php
+}?>
