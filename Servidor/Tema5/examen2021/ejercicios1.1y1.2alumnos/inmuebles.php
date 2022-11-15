3bb55fa67890f37f83e80dbc24aaa0c2ef7f34d3
HeciarMask <franheciar@gmail.com>
2022-11-15 14:55:27 +0100

    eo

--- Servidor/Tema5/examen2021/ejercicios1.1y1.2alumnos/inmuebles.php
+++ Servidor/Tema5/examen2021/ejercicios1.1y1.2alumnos/inmuebles.php
@@ -14,14 +14,49 @@ Tipo de vivienda:</td><td>
     
 <!-- <input type="checkbox" name="tipo" value="indice">Valor</input> -->
 <?php
-    
+    $dwes=new mysqli("localhost","root","","ejercicio1");
+    $sql1="SELECT nombre, id FROM tipos_vivienda";
+    $resultado1=$dwes->query($sql1);
+    while ($fila1=$resultado1->fetch_assoc())
+    {
+        $valor1=$fila1["nombre"];
+        $ind1 = $fila1["id"];
+        echo "<input type=\"checkbox\" name=\"tipo[]\" value=\"$ind1\">$valor1<br>";
+    }
 ?>
 </td></tr>
 
 <tr><td>Localidad:</td><td>
-Localidades </td></tr>
+<select name="localidad">
+<option value="todos">Todos</option>
+<?php
 
-<tr><td>Precio entre</td><td> <input type="text" name="minimo" /> y <input type="text" name="maximo"  /></td></tr>
+ $sql2="SELECT nombre, id FROM localidades";
+ $resultado2=$dwes->query($sql2);
+ while ($fila2=$resultado2->fetch_assoc())
+ {
+     $valor2=$fila2["nombre"];
+     $ind2= $fila2["id"];
+     echo "<option value=\"$ind2\">$valor2</option>";
+ }
+?>
+</select>
+</td></tr>
+<?php
+ $min = 9999999999;
+ $max = 0;
+ $sql3="SELECT MAX(precio) AS maxi, MIN(precio) AS mini FROM propiedades";
+ $resultado3=$dwes->query($sql3);
+ $fila3=$resultado3->fetch_assoc();
+ $min = $fila3["mini"];
+ $max = $fila3["maxi"];     
+
+echo "<tr><td>Precio entre</td><td> <input type=\"text\" name=\"minimo\" value=\"$min\"/> y
+<input type=\"text\" name=\"maximo\" value=\"$max\"/></td></tr>";
+$dwes->close();
+?>
+
+
 <tr><td>Ordenar precios </td><td><input type="radio" name="orden" checked value="1">Ascendente<input type="radio" name="orden" value="2">Descendente</td></tr>
 <tr><td></td><td><input type="submit" name="submit" value="Buscar" /></td></tr>
 
--- Servidor/Tema5/examen2021/ejercicios1.1y1.2alumnos/listado.php
+++ Servidor/Tema5/examen2021/ejercicios1.1y1.2alumnos/listado.php
@@ -8,7 +8,45 @@
 <h1>Gestión de Inmuebles</h1>
 <p><a href="inmuebles.php">Nueva búsqueda</a></p>
 <table border="1"><tr><th>Localidad</th><th>Domicilio</th><th>Tipo de Vivienda</th><th>Precio</th></tr>	
-<tr><td></td><td></td><td></td><td></td></tr>
+
+<?php
+
+extract($_POST);
+$dwes=new mysqli("localhost","root","","ejercicio1");
+
+if(!empty($tipo)){
+    $cadTipo = implode(' \', \'', $tipo);
+}else{
+
+}
+
+
+
+    if($localidad == "todos"){
+        $sql1="SELECT localidad, domicilio, tipo, precio FROM propiedades WHERE vendida='NO' AND tipo IN  (\'$cadTipo\')";
+    }else{
+        $sql1="SELECT localidad, domicilio, tipo, precio FROM propiedades WHERE vendida='NO' AND localidad=\'$localidad\'  AND tipo IN  (\'$cadTipo\')";
+    }
+    $resultado1=$dwes->query($sql1);
+    while ($fila1=$resultado1->fetch_assoc())
+    {
+        $idLoc = $fila1["localidad"];
+        $sql2="SELECT nombre FROM localidades WHERE id=\'$idLoc\'";
+        $resultado2=$dwes->query($sql2);
+        $fila2=$resultado2->fetch_assoc();
+
+        $idTipo  = $fila1["tipo"];
+        $sql3="SELECT nombre FROM tipos_vivienda WHERE id=\'$idTipo\'";
+        $resultado3=$dwes->query($sql3);
+        $fila3=$resultado3->fetch_assoc();
+
+        echo "<tr><td>".$fila2["nombre"]."</td>
+                        <td>".$fila1["domicilio"]."</td>
+                        <td>".$fila3["nombre"]."</td>
+                        <td>".$fila1["precio"]."</td></tr>";
+    }
+
+?>
 </table>
 </body>
 </html>
