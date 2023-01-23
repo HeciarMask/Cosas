5ef7dc1471157ca5919e4daaaf02deff2cef5add
HeciarMask <franheciar@gmail.com>
2023-01-23 14:56:22 +0100

    lunes

--- Servidor/Tema8/examen2017/Model/curso.php
+++ Servidor/Tema8/examen2017/Model/curso.php
@@ -3,13 +3,12 @@
 class Curso {
     protected $curso;   
     
-    public function getcurso() {return $this->curso; }
-
-  
     public function __construct($row) {
         $this->curso = $row['curso'];
        		
     }
+    
+    public function getcurso() {return $this->curso; }
 }
 
 ?>
