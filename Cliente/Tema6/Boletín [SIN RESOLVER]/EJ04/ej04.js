86bfd12ae431f653d114704db7d1f597bb89b013
HeciarMask <franheciar@gmail.com>
2023-01-27 14:56:09 +0100

    viernes cliente

--- Cliente/Tema6/Boletín [SIN RESOLVER]/EJ04/ej04.js
+++ Cliente/Tema6/Boletín [SIN RESOLVER]/EJ04/ej04.js
@@ -1,1 +1,22 @@
+document.getElementById("boton").addEventListener("click", crearTabla);
 
+function crearTabla() {
+	let filas = formulario.filas.value;
+	let columnas = formulario.columnas.value;
+	let tabla = document.createElement("table");
+
+	let contador = 1;
+
+	for (let i = 0; i < filas; i++) {
+		let fila = tabla.insertRow(i);
+		for (let j = 0; j < columnas; j++) {
+			let celda = fila.insertCell(j);
+			celda.append(document.createTextNode(contador));
+			contador++;
+		}
+	}
+
+	document.getElementById("tabla").append(tabla);
+	formulario.columnas.value = "";
+	formulario.filas.value = "";
+}
