7cb83c38caac673a3b3752643090261c1b948269
HeciarMask <franheciar@gmail.com>
2023-02-07 13:01:33 +0100

    a poquito of laravel

---
+++ HDL/OcioSevillaTablasAux/src/java/g_ocio.java
@@ -0,0 +1,230 @@
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.Map;
+import javafx.event.ActionEvent;
+import javax.faces.component.html.HtmlDataTable;
+public class g_ocio {
+
+    public static Connection Conn;
+    private ResultSet rsOcio;
+    private ResultSet rsFPago;
+    private HtmlDataTable tabla;
+    private String comprobar;
+    public static String idLocal;
+    //para las búsquedas
+    String sConsulta="select * from locales_zonas";
+    String sWhere=" WHERE true";
+    String sOrden=" ORDER BY id";
+    private String snombre_Busc;
+    private String szona_Busc;
+    private String[] sfpago_Busc;
+    private String sdireccion_Busc;
+    ///resultado de la búsqueda
+    private String sinfo_Sel;
+   
+    
+    public g_ocio() {
+        Conn=MySQL_Util.Conectar("localhost", "root", "", "ocio");
+    }
+
+    /**
+     * @return the rsOcio
+     */
+    public ResultSet getRsOcio() {
+        String cadenaSql=sConsulta+sWhere+sOrden;
+        rsOcio=MySQL_Util.Sel_Consulta(Conn, cadenaSql);
+        return rsOcio;
+    }
+    public String seleccionLista_Del(){
+        String sId_borrar=ObtenerID();
+    //Borrar en la tabla locales_formas_pago
+    String borrarHijos="DELETE FROM locales_formas_pago ";
+    borrarHijos+=" WHERE ID_LOCAL="+sId_borrar;
+    MySQL_Util.Ej_ConsultaAccion(Conn, borrarHijos);
+    //borrar en la tabla locales
+    String borrarPadre="DELETE FROM locales WHERE ID="+sId_borrar;
+    MySQL_Util.Ej_ConsultaAccion(Conn, borrarPadre);
+    return "index";
+    }
+    public String obtenerDatosLocal(){
+     idLocal=ObtenerID();   
+    return "m_local";
+    }
+    public String nuevoLocal(){
+        idLocal="";
+        return "a_local";
+    }
+    public String ObtenerID(){
+     Map <String,Object> fila;   
+     fila= (Map <String,Object>)tabla.getRowData();
+     String devuelve=fila.get("ID").toString();
+     return devuelve;
+    }
+    /**
+     * @param rsOcio the rsOcio to set
+     */
+    public void setRsOcio(ResultSet rsOcio) {
+        this.rsOcio = rsOcio;
+    }
+
+    /**
+     * @return the rsFPago
+     */
+    public ResultSet getRsFPago() {
+        String sqlPago;
+        try{
+        sqlPago="SELECT fp.NOMBRE FROM formas_pago fp INNER JOIN locales_formas_pago lfp ON fp.ID=lfp.ID_FORMA_PAGO ";
+        sqlPago+=" WHERE lfp.ID_LOCAL="+rsOcio.getString("ID") ;
+        rsFPago=MySQL_Util.Sel_Consulta(Conn, sqlPago);
+        }
+        catch (SQLException ex){
+            return null;
+        }
+        return rsFPago;
+    }
+
+    /**
+     * @return the tabla
+     */
+    public HtmlDataTable getTabla() {
+        return tabla;
+    }
+
+    /**
+     * @param tabla the tabla to set
+     */
+    public void setTabla(HtmlDataTable tabla) {
+        this.tabla = tabla;
+    }
+
+    /**
+     * @return the comprobar
+     */
+    public String getComprobar() {
+        return comprobar;
+    }
+
+    /**
+     * @return the snombre_Busc
+     */
+    public String getSnombre_Busc() {
+        return snombre_Busc;
+    }
+
+    /**
+     * @param snombre_Busc the snombre_Busc to set
+     */
+    public void setSnombre_Busc(String snombre_Busc) {
+        this.snombre_Busc = snombre_Busc;
+    }
+
+    /**
+     * @return the szona_Busc
+     */
+    public String getSzona_Busc() {
+        return szona_Busc;
+    }
+
+    /**
+     * @param szona_Busc the szona_Busc to set
+     */
+    public void setSzona_Busc(String szona_Busc) {
+        this.szona_Busc = szona_Busc;
+    }
+
+    /**
+     * @return the sfpago_Busc
+     */
+    public String[] getSfpago_Busc() {
+        return sfpago_Busc;
+    }
+
+    /**
+     * @param sfpago_Busc the sfpago_Busc to set
+     */
+    public void setSfpago_Busc(String[] sfpago_Busc) {
+        this.sfpago_Busc = sfpago_Busc;
+    }
+
+    /**
+     * @return the sdireccion_Busc
+     */
+    public String getSdireccion_Busc() {
+        return sdireccion_Busc;
+    }
+
+    /**
+     * @param sdireccion_Busc the sdireccion_Busc to set
+     */
+    public void setSdireccion_Busc(String sdireccion_Busc) {
+        this.sdireccion_Busc = sdireccion_Busc;
+    }
+
+    /**
+     * @return the sinfo_Sel
+     */
+    public String getSinfo_Sel() {
+        String sDevuelve= "Número de locales encontrados:";
+        int numero=MySQL_Util.Numero_Registros(rsOcio);
+        sinfo_Sel=sDevuelve+numero;
+        return sinfo_Sel;
+    }
+
+    /**
+     * @param sinfo_Sel the sinfo_Sel to set
+     */
+    public void setSinfo_Sel(String sinfo_Sel) {
+        this.sinfo_Sel = sinfo_Sel;
+    }
+    public ArrayList getListaZonas(){
+        String cadena="SELECT id,nombre FROM zonas order by nombre";
+        String sPrimeraOpcion="Indiferente";
+        return MySQL_Util.Llenar_Lista_Busquedas(g_ocio.Conn, cadena,sPrimeraOpcion);
+    }
+    public ArrayList getListaFPago(){
+        String cadena="SELECT id,nombre FROM formas_pago";
+        return MySQL_Util.Llenar_Lista(g_ocio.Conn, cadena);
+    }
+    public void buscar_local(ActionEvent event){
+        if (snombre_Busc.trim().length()>0){
+            sWhere+=" AND nombre LIKE '%"+snombre_Busc+"%'";
+        }
+        if (sdireccion_Busc.trim().length()>0){
+            sWhere+=" AND direccion LIKE '%"+sdireccion_Busc+"%'";
+        }
+        if (!szona_Busc.equals("-1")){
+            sWhere+="  AND idzona="+szona_Busc;
+        }
+        if (sfpago_Busc.length>0)   {
+            sWhere+=" AND id in (select ID_LOCAL FROM locales_formas_pago where ID_FORMA_PAGO in"+MySQL_Util.implode(sfpago_Busc)+")";
+        
+        }
+    }
+    public void limpiar_buscar_local(ActionEvent event){
+        snombre_Busc="";
+        sdireccion_Busc="";
+        szona_Busc="-1";
+        sfpago_Busc=null;
+        sWhere=" WHERE true";
+    }
+     public String gestion_Zonas() {
+
+        g_tablas_aux.stabla = "zonas";
+
+        g_tablas_aux.stitulo = "Gestión de Zonas:";
+
+return "g_tablas_aux";
+
+}
+
+public String gestion_FormasPago() {
+
+        g_tablas_aux.stabla = "formas_pago";
+
+        g_tablas_aux.stitulo = "Gestión de Formas de Pago:";
+
+return "g_tablas_aux";
+    
+}
+    
+}
---
+++ HDL/OcioSevillaTablasAux/web/g_tablas_aux.jsp
@@ -0,0 +1,73 @@
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<%@taglib prefix="f" uri="http://java.sun.com/jsf/core"%>
+<%@taglib prefix="h" uri="http://java.sun.com/jsf/html"%>
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
+"http://www.w3.org/TR/html4/loose.dtd">
+
+<f:view>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+<title><h:outputText value="#{g_tablas_aux.stitulo}"/></title>
+</head>
+<body>
+<h1><h:outputText value="#{g_tablas_aux.stitulo}"/></h1>
+<h:form>
+<h:dataTable border="3" value="#{g_tablas_aux.rsTabla}" binding="#{g_tablas_aux.tabla}"
+             var="reg">
+    <h:column>
+        <f:facet name="header">
+            <h:outputText value="Borrar"/>
+        </f:facet>
+        <h:commandButton value="X" onclick="return confirm('¿Deseas BORRAR esa fila?')"
+                         actionListener="#{g_tablas_aux.borrar_Fila}" />
+    </h:column>
+    <h:column>
+        <f:facet name="header">
+            <h:outputText value="Id"/>
+        </f:facet>
+        <h:outputText value="#{reg.ID}"/>
+    </h:column>
+    <h:column>
+        <f:facet name="header">
+            <h:outputText value="Nombre"/>
+        </f:facet>
+        <h:outputText value="#{g_tablas_aux.iterar_Nombre}"/>
+        <h:inputText id="mod" size="45" binding="#{g_tablas_aux.nombre_mod}" 
+                     required="true" 
+                     requiredMessage="No puedes Modificar a Vacio">
+
+        </h:inputText>
+        <h:message for="mod" style="color:red"/>
+        </h:column>
+    <h:column>
+        <f:facet name="header">
+            <h:outputText value="Modificación"/>
+        </f:facet>
+        <h:commandButton value="Modificar" 
+                         actionListener="#{g_tablas_aux.modificar_Fila}"/>
+    </h:column>
+</h:dataTable>
+</h:form>
+<br>
+<h:form>
+<table border="3">
+<tr>
+<td> <h:commandButton value="Añadir" actionListener="#{g_tablas_aux.insertar_Fila}"/></td>
+<td>
+<h:inputText id="nuevo" size="44" value="#{g_tablas_aux.nombre}" 
+             required="true" requiredMessage="No puedes Añadir Vacio">
+<f:validateLength minimum="2"/>
+</h:inputText>
+<h:message for="nuevo" style="color:red"/>
+</td>
+</tr>
+</table>
+<br><br>
+<h:commandButton value="<-- Volver" action="index" immediate="true"/>
+</h:form>
+</body>
+</html>
+</f:view>
+
+
