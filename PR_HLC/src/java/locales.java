ecd0e994ba53f12414586c23c5a2d38dc87179cd
Francisco Lopez <franheciar@gmail.com>
2023-02-09 19:30:25 +0100

    empiezo hlc

---
+++ PR_HLC/src/java/locales.java
@@ -0,0 +1,138 @@
+
+import java.sql.Connection;
+import java.util.ArrayList;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author alumno
+ */
+public class locales {
+
+   private String nombre;
+   private String zona;
+   private String direccion;
+   private String[] formas_pago;
+   private String mensaje;
+    public locales() {
+        if (!g_ocio.idLocal.equals(""))
+            recoge_datos(g_ocio.idLocal);
+        
+    }
+public void recoge_datos(String pIdLocal){
+nombre=MySQL_Util.ObtenerDato("locales", "id", pIdLocal, "nombre");
+direccion=MySQL_Util.ObtenerDato("locales", "id", pIdLocal, "direccion");
+zona=MySQL_Util.ObtenerDato("locales", "id", pIdLocal, "zona");
+//Obtener las formas de pago del local
+String cadsql="SELECT id_forma_pago FROM locales_formas_pago";
+cadsql+=" WHERE ID_LOCAL="+pIdLocal;
+formas_pago=MySQL_Util.Llenar_Seleccionados(g_ocio.Conn,cadsql,"ID_FORMA_PAGO");
+}
+    /**
+     * @return the nombre
+     */
+    public String getNombre() {
+        return nombre;
+    }
+
+    /**
+     * @param nombre the nombre to set
+     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    /**
+     * @return the zona
+     */
+    public String getZona() {
+        return zona;
+    }
+
+    /**
+     * @param zona the zona to set
+     */
+    public void setZona(String zona) {
+        this.zona = zona;
+    }
+
+    /**
+     * @return the direccion
+     */
+    public String getDireccion() {
+        return direccion;
+    }
+
+    /**
+     * @param direccion the direccion to set
+     */
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    /**
+     * @return the formas_pago
+     */
+    public String[] getFormas_pago() {
+        return formas_pago;
+    }
+
+    /**
+     * @param formas_pago the formas_pago to set
+     */
+    public void setFormas_pago(String[] formas_pago) {
+        this.formas_pago = formas_pago;
+    }
+    public ArrayList getListaZonas(){
+        String cadena="SELECT id,nombre FROM zonas order by nombre";
+        return MySQL_Util.Llenar_Lista(g_ocio.Conn, cadena);
+    }
+    public ArrayList getListaFPago(){
+        String cadena="SELECT id,nombre FROM formas_pago";
+        return MySQL_Util.Llenar_Lista(g_ocio.Conn, cadena);
+    }
+    public String guardar_L(){
+        //INsert en locales
+        String insert1="INSERT INTO locales(NOMBRE,ZONA,DIRECCION) VALUES('";
+        insert1+=nombre+"',"+zona+",'"+direccion+"')";       
+        int idNuevoLocal=MySQL_Util.Ej_Consulta_Id_Auto(g_ocio.Conn,insert1);
+        //INsert en locales_formas_pago
+        mensaje=idNuevoLocal+"";
+        for (int i=0;i<formas_pago.length;i++){
+            String insert2="INSERT INTO locales_formas_pago ";
+            insert2+=" VALUES("+idNuevoLocal+","+formas_pago[i]+")";
+            MySQL_Util.Ej_ConsultaAccion(g_ocio.Conn, insert2);
+        
+        }
+        return "index";
+    }
+
+    /**
+     * @return the mensaje
+     */
+    public String getMensaje() {
+        return mensaje;
+    }
+    public String modificar_local(){
+        //primero modifico en locales
+    String cadsql1="UPDATE locales SET nombre='"+nombre+"',direccion='";
+    cadsql1+=direccion+"', zona="+zona+" WHERE id="+g_ocio.idLocal;
+    MySQL_Util.Ej_ConsultaAccion(g_ocio.Conn, cadsql1);
+    //para modificar las formas de pago, primero borro las que había
+    String cadBorra="DELETE FROM locales_formas_pago WHERE ID_LOCAL="+g_ocio.idLocal;
+    MySQL_Util.Ej_ConsultaAccion(g_ocio.Conn, cadBorra);
+    //Y a continuación meto las nuevas
+    for (int i=0;i<formas_pago.length;i++){
+            String insert2="INSERT INTO locales_formas_pago ";
+            insert2+=" VALUES("+g_ocio.idLocal+","+formas_pago[i]+")";
+            MySQL_Util.Ej_ConsultaAccion(g_ocio.Conn, insert2);
+        
+        }
+    return "index";
+    }
+}
